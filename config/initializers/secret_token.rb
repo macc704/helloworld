16e864d288103fc91330097a0702418aa25450bc
Yoshiaki Matsuzawa <yoshiaki.matsuzawa@gmail.com>
2013-10-04 09:27:07 +0000

    Generate a new Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helloworld::Application.config.secret_key_base = '55c88d581fad578773052848c0a97ce32641a187fe9bcdf366953ca136bd479c2752d2d37648c3fc1d3571834147371e4d9bad051901a3c3386895653e3da669'
